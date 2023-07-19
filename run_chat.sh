a243bd2e54e55dfb91f9f851b91764090dd430cd
Nick <nick@lambdal.com>
2023-07-18 23:52:53 -0700

    Fix docker run command

--- run_chat.sh
+++ run_chat.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # cd into the llama directory first
-sudo docker run -it --rm -v $(pwd):/workspace/llama -w /workspace/llama bash run_chat_inside_docker.sh
+sudo docker run -it --rm -v $(pwd):/workspace/llama -w /workspace/llama pytorch/pytorch:latest bash run_chat_inside_docker.sh
